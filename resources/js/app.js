249ccedd04376dc1eb3b873835b35e2f8079772b
rw09 <rodrigosouza170487@gmail.com>
2024-03-14 13:58:49 -0300

    Adicionando Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,20 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { md1 } from 'vuetify/blueprints'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    blueprint: md1,
+  });
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +27,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
